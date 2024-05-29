4f788e9b8c0540bff75c154b80c2ca655360bd53
İsmet Onur ŞAHİN <110236136+ismetonursahin@users.noreply.github.com>
2024-05-30 01:41:20 +0300

    customer

--- VeterinaryApp/src/pages/Customer/Customer.jsx
+++ VeterinaryApp/src/pages/Customer/Customer.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { IoMdAdd } from "react-icons/io";
 
 function Customer() {
   const [customer, setCustomer] = useState();
+  const [update, setUpdate] = useState(false);
   const [newCustomer, setNewCustomer] = useState({
     name: "",
     address: "",
@@ -14,8 +16,9 @@ function Customer() {
   useEffect(() => {
     axios
       .get(import.meta.env.VITE_VET_API_BASEURL + "/api/v1/customers")
-      .then((res) => setCustomer(res.data.content));
-  }, []);
+      .then((res) => setCustomer(res.data.content))
+      .then(() => setUpdate(true));
+  }, [update]);
 
   const handleNewCustomerInputChange = (e) => {
     const { name, value } = e.target;
@@ -23,6 +26,15 @@ function Customer() {
       ...prev,
       [name]: value,
     }));
+  };
+
+  const handleAddNewCustomer = () => {
+    axios
+      .post(
+        import.meta.env.VITE_VET_API_BASEURL + "/api/v1/customers",
+        newCustomer
+      )
+      .then(setUpdate(false));
   };
 
   useEffect(() => {
@@ -39,16 +51,56 @@ function Customer() {
           ))}
         </div>
       </div>
-      <div>
+      <div className="bg-slate-400 mt-2 ">
         <h2>Müşteri Ekle</h2>
-        <input
-          type="text"
-          name="name"
-          value={newCustomer.name}
-          placeholder="Müşteri Adı"
-          onChange={handleNewCustomerInputChange}
-        />
+        <div className="flex flex-col gap-2 w-44 items-center">
+          <input
+            type="text"
+            name="name"
+            value={newCustomer.name}
+            placeholder="Müşteri Adı"
+            onChange={handleNewCustomerInputChange}
+          />{" "}
+          <input
+            type="text"
+            name="phone"
+            value={newCustomer.phone}
+            placeholder="Müşteri Telefon"
+            onChange={handleNewCustomerInputChange}
+          />{" "}
+          <input
+            type="text"
+            name="email"
+            value={newCustomer.email}
+            placeholder="Müşteri E-mail"
+            onChange={handleNewCustomerInputChange}
+          />{" "}
+          <input
+            type="text"
+            name="address"
+            value={newCustomer.address}
+            placeholder="Müşteri Adres"
+            onChange={handleNewCustomerInputChange}
+          />{" "}
+          <input
+            type="text"
+            name="city"
+            value={newCustomer.city}
+            placeholder="Şehir"
+            onChange={handleNewCustomerInputChange}
+          />
+          <button
+            onClick={handleAddNewCustomer}
+            className="flex justify-center items-center w-24 p-1 bg-green-400 gap-2 rounded-lg"
+          >
+            <div>
+              <IoMdAdd />
+            </div>
+            <div>Ekle</div>
+          </button>
+        </div>
       </div>
+      {newCustomer.name}
     </div>
   );
 }
